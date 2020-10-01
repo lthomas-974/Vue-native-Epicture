6730dc22b448c320a8cba59eea6b4aed69284f23
Yobar73 <bthl.yohann73@gmail.com>
2020-10-01 10:32:28 +0200

    update store

--- store/index.js
+++ store/index.js
@@ -5,22 +5,44 @@ Vue.use(Vuex);
 
 const store = new Vuex.Store({
   state: {
-    UserData:{},
-    MyFavorites:{}
+    UserData: {},
+    MyFavorites: {}
   },
 
   mutations: {
     setUserData(state, payload) {
-        this.state.UserData = payload
-        console.log("------------> In Store")
-        console.log(state.UserData)
+      this.state.UserData = payload
+      console.log("------------> In Store")
+      console.log(state.UserData)
     },
     setMyFavorites(state, payload) {
       this.state.MyFavorites = payload
       console.log("------------> In Store")
-      console.log(state.MyFavorites)
+      console.log(state)
+    },
   },
+
+  actions: {
+    async updateMyFavorites(context) {
+      try {
+        const imgurApiCall = await fetch(
+          `https://api.imgur.com/3/account/${store.state.UserData.params.account_username}/favorites`,
+          {
+            methods: "GET",
+            headers: {
+              authorization: "Bearer " + store.state.UserData.params.access_token,
+            },
+          }
+        );
+        const pictures = await imgurApiCall.json();
+        console.log(pictures);
+        store.commit("setMyFavorites", pictures.data);
+      } catch (err) {
+        console.log("Error fetching data-----------", err);
+      }
+    }
   }
+
 });
 
 export default store;
